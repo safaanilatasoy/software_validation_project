bd4ca3ebb1b6d7a32f3615e73a6624b667d89ad1
safaanilatasoy <safaanilatasoy@gmail.com>
2021-12-24 21:31:51 +0300

    main

--- elibrary-app/routes/web.php
+++ elibrary-app/routes/web.php
@@ -4,28 +4,18 @@ use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
 
 #Route::get('/',"BookController@index"); 
-
-Route::get('/edit/{id}',"BookController@edit"); 
-Route::get('/show/{id}',"BookController@show"); 
-Route::get('/create',"BookController@create"); 
-Route::post('/store',"BookController@store"); 
-Route::post('/update/{id}',"BookController@update"); 
+#Route::get('/edit/{id}',"BookController@edit"); 
+#Route::get('/show/{id}',"BookController@show"); 
+#Route::get('/create',"BookController@create"); 
+#Route::post('/store',"BookController@store"); 
+#Route::post('/update/{id}',"BookController@update"); 
 
 Route::get('/', [BookController::class, 'index']);
+Route::get('/edit{id}', [BookController::class, 'edit']);
+Route::get('/show{id}', [BookController::class, 'show']);
+Route::get('/create', [BookController::class, 'create']);
+Route::post('/store', [BookController::class, 'store']);
+Route::post('/update{id}', [BookController::class, 'update']);
+
